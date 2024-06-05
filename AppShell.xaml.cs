8e8d4d1aba05bb9dfd5d1b0d3e61024ce59e3253
DontsaClaudia <donstsaclaudia@gmail.com>
2024-06-05 10:45:21 +0200

    Ajoutez des fichiers projet.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace _3ILPark_Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace _3ILPark_Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
